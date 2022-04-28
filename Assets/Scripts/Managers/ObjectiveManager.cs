d88377a829cc6042683e305cafae8dda0c754e41
Bradley Watkins <watkinsbradley01@gmail.com>
2022-04-28 12:55:31 +0100

    collectible spawns randomly around play area

--- Assets/Scripts/Managers/ObjectiveManager.cs
+++ Assets/Scripts/Managers/ObjectiveManager.cs
@@ -8,5 +8,24 @@ public class ObjectiveManager : MonoBehaviour
     [SerializeField] private Transform objective;
     [SerializeField] private float heightFromPlatform;
 
+    private int lastLocation = -1;
+    private int newLocation;
+    private Vector2 pos;
 
+    private void Start()
+    {
+        MoveObjective();
+    }
+    public void MoveObjective()
+    {
+        newLocation = Random.Range(0, platforms.Length);
+        while (lastLocation == newLocation)
+        {
+            newLocation = Random.Range(0, platforms.Length);
+        }
+
+        pos = platforms[newLocation].position;
+        pos.y += heightFromPlatform;
+        objective.position = pos;
+    }
 }
