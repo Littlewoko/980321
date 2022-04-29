cd756b79deafad88ea6e780ef68c5de85fc78988
Bradley Watkins <watkinsbradley01@gmail.com>
2022-04-29 19:55:51 +0100

    Moving platforms

--- Assets/Scripts/Managers/DifficultyManager.cs
+++ Assets/Scripts/Managers/DifficultyManager.cs
@@ -5,16 +5,62 @@ using UnityEngine;
 public class DifficultyManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] enemies;
+    [SerializeField] private float diffIncreaseRate;
+    [SerializeField] private PlatformMovement[] platforms;
 
     private List<GameObject> inactiveEnemies = new List<GameObject>();
+    private List<PlatformMovement> stationaryPlatforms = new List<PlatformMovement>();
     private int enemyToActivate;
+    private int platformToMove;
+
+    private float curChance;
 
     private void Awake()
     {
         inactiveEnemies.AddRange(enemies);
+        stationaryPlatforms.AddRange(platforms);
     }
 
-    public void IncreaseDifficulty()
+    private void Start()
+    {
+        curChance = 0.5f;
+        InvokeRepeating("IncreaseDifficultyViaTime",
+            diffIncreaseRate, diffIncreaseRate);
+    }
+
+    public void IncreaseDifficultyViaTime()
+    {
+        if (stationaryPlatforms.Count == 0) return;
+
+        platformToMove = Random.Range(0, stationaryPlatforms.Count);
+
+        if (stationaryPlatforms[platformToMove].movingX)
+        {
+            stationaryPlatforms[platformToMove].StartYMotion();
+            stationaryPlatforms.RemoveAt(platformToMove);
+            return;
+        }
+
+        if (stationaryPlatforms[platformToMove].movingY)
+        {
+            stationaryPlatforms[platformToMove].StartXMotion();
+            stationaryPlatforms.RemoveAt(platformToMove);
+            return;
+        }
+
+        if (Random.value >= curChance)
+        {
+            stationaryPlatforms[platformToMove].StartYMotion();
+            curChance += 0.1f;
+        }
+        else
+        {
+            stationaryPlatforms[platformToMove].StartXMotion();
+            curChance -= 0.1f;
+        }
+    }
+
+    public void IncreaseDifficultyViaPorgression()
     {
         if (inactiveEnemies.Count == 0) return;
 
@@ -22,4 +68,6 @@ public class DifficultyManager : MonoBehaviour
         inactiveEnemies[enemyToActivate].SetActive(true);
         inactiveEnemies.RemoveAt(enemyToActivate);
     }
+
+    
 }
---
+++ Assets/Scripts/Movement Scripts/PlatformMovement.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlatformMovement : MonoBehaviour
+{
+    [SerializeField] private float yMoveDist;
+    [SerializeField] private float xMoveDist;
+    [SerializeField] private float movementTime;
+    private Transform tr;
+    private Sequence ySequence;
+    private float yStart;
+    private float yTop;
+    private float yBottom;
+    private float xStart;
+    private float xTop;
+    private float xBottom;
+    private Sequence xSequence;
+
+    public bool movingY { private set; get; }
+    public bool movingX { private set; get; }
+
+    private void Awake()
+    {
+        tr = transform;
+        movingX = false;
+        movingY = false;
+    }
+
+    public void StartYMotion()
+    {
+        if (movingX) return;
+
+        yStart = tr.localPosition.y;
+        yTop = yStart + yMoveDist;
+        yBottom = yStart - yMoveDist;
+
+        movingY = true;
+        tr.DOLocalMoveY(yTop, movementTime / 4f).SetEase(Ease.InOutCubic)
+            .OnComplete(StartYSequence);
+    }
+
+    public void StartXMotion()
+    {
+        if (movingY) return;
+
+        xStart = tr.localPosition.x;
+        xTop = xStart + xMoveDist;
+        xBottom = xStart - xMoveDist;
+
+        movingX = true;
+        tr.DOLocalMoveX(xTop, movementTime / 4f).SetEase(Ease.InOutCubic)
+            .OnComplete(StartXSequence);
+    }
+
+    private void StartYSequence()
+    {
+        ySequence = DOTween.Sequence();
+
+        ySequence.Append(tr.DOLocalMoveY(yBottom, movementTime / 2f))
+            .SetEase(Ease.InOutCubic);
+        
+
+        ySequence.SetLoops(-1, LoopType.Yoyo);
+    }
+
+    private void StartXSequence()
+    {
+        xSequence = DOTween.Sequence();
+
+        xSequence.Append(tr.DOLocalMoveX(xBottom, movementTime / 2f))
+            .SetEase(Ease.InOutCubic);
+
+
+        xSequence.SetLoops(-1, LoopType.Yoyo);
+    }
+}
